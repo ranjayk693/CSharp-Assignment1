e1102d69e4e67e6786e8ebca02088b19e6bdd774
Ranjay kumar <ranjayk693@gmail.com>
2024-03-06 22:24:16 +0530

    inital commit

--- Program.cs
+++ Program.cs
@@ -6,8 +6,7 @@ public class Employee
     private string Name;
     private string DepartmentName;
 
-    //event in EventHandler Generics of tyoe string
-    public event EventHandler<string> MethodCalled;
+    
     //constructor
     public Employee(string id, string name, string departmentName)
     {
@@ -17,53 +16,37 @@ public class Employee
     }
 
     // Return the ID with event calling
-    public string GetId()
-    {
-        OnMethodCalled("GetId() method called");
+    public string GetId(){
         return Id;
     }
 
     // Return the name with event calling
-    public string GetName()
-    {
-        OnMethodCalled("GetName method called");
+    public string GetName(){ 
         return Name;
     }
 
     // Return the Department with event calling
-    public string GetDepartmentName()
-    {
-        OnMethodCalled("GetDepartmentName method called");
+    public string GetDepartmentName(){
         return DepartmentName;
     }
 
     // Update the ID 
-    public void UpdateId(string id)
-    {
+    public void UpdateId(string id){
         Id = id;
-        OnMethodCalled("UpdateId method called");
     }
 
     // Update the name
-    public void UpdateName(string name)
-    {
+    public void UpdateName(string name){
         Name = name;
-        OnMethodCalled("UpdateName() method called");
     }
 
     // Update the Department
-    public void UpdateDepartmentName(string departmentName)
-    {
+    public void UpdateDepartmentName(string departmentName){
         DepartmentName = departmentName;
-        OnMethodCalled("UpdateDepartmentName method called");
     }
 
 
-    //This method call the MethodCalled event whichever function contain this.
-    public void OnMethodCalled(string methodName)
-    {
-        MethodCalled?.Invoke(this, methodName);  //It will invoke the MethodCalled event with the current instance with method name
-    }
+ 
 }
 
 // Main program
@@ -81,13 +64,7 @@ public class Program
         string departmentName = Console.ReadLine();
 
         //Creating the object for Employee class
-        Employee employee = new Employee(id, name, departmentName);
-
-        //whenever the method is called then this will print the OnMethodCalled function message
-        employee.MethodCalled += (data, method) =>
-        {
-            Console.WriteLine(method);
-        };
+        Employee employee = new Employee(id, name, departmentName);        
 
         //Printing the console data
         Console.WriteLine("Employee Details:");
